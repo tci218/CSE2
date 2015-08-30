26bef83ed60786742fdef6dc6eea8d5fc9c31373
tci218 <tci218@lehigh.edu>
2015-08-30 19:45:35 +0000

    submitting WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,22 @@
+//Trent Insull
+//8/30/2015
+//CSE002-310
+
+//define class
+public class WelcomeClass{
+   
+   //add main method 
+    public static void main(String[] args){
+        //print statements and boxes around them
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-T--C--I--2--1--8-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("This summer I worked at a summer camp. I was with 7-8 year old kids and we played sports all day");
+
+    }
+}
